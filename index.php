082648758da3bba87e055a974405982676431ce5
victoriaspacheco <victoriaspacheco1@gmail.com>
2021-12-03 11:48:44 -0300

    layout pronto

--- index.php
+++ index.php
@@ -8,29 +8,20 @@
 		<script type="text/javascript" src="query.maskedinput.js"></script>
 		<script type="text/javascript" src="funcoesN2.js"></script>
 				
-		<h1>Trabalho N2 TESTE2</h1>
+		<h1>Trabalho N2</h1>
 		<h2>Pesquisa e Armazenamento de Dados</h2>
 		<meta charset="utf-8">
 		<title>Trabalho N2</title>
 	</head>
-	<body style="font-family: helvetica">
-		
-
-		<form name="cadastropessoa" action="listabancoN2.php" method="post" onsubmit="return validarCadastro();">
-							
-		<div class="label-nome">		
-					<label>Nome:
-					<input type="text" name="nome" id ="nome" maxlength="100"  placeholder = "Seu nome..."  />
-					</label>					
-		</div>	
-		<div>
-		<input type="hidden" name="acao" value="inserir" />
+	<body style="font-family: helvetica">	
+		<div class="menu3">
+			<form name="cadastropessoa" action="listabancoN2.php" method="post" onsubmit="return validarCadastro();">
+					<input type="text" name="nome" id ="nome" maxlength="100"  placeholder = " Insira um nome..."  />							
+					<input type="hidden" name="acao" value="inserir" />
 					<input class="botao" type="submit" name="botao" value="Cadastrar">
-					<input onclick="window.location.href = 'listabancoN2.php'" style="text-align:center" class="botao2" name="botao" value="Listar" />
-		</div>
-			
+					<input class="botao" onclick="window.location.href = 'listabancoN2.php'" style="text-align:center" class="botao2" name="botao" value="Listar" />						
 			</form>
-		
+		</div>
 	</body>
 	
 </html>
--- indexprincipal.php
+++ indexprincipal.php
@@ -6,18 +6,16 @@
 	<meta charset="utf-8">
 	<title>Trabalho N2 Pesquisa e Armazenamento de Dados</title>
 </head>
-	<body>		
 	<h1>Trabalho N2</h1>
-		<h2>Pesquisa e Armazenamento de Dados</h2>
-		<div class="meunome">
-		<h3>Luis Mauricio C. Pires & Victória da Silva Pacheco</h3>
-		</div>
+	<h2>Pesquisa e Armazenamento de Dados</h2>
+	<div class="meunome">
+	<h3>Luis Mauricio C. Pires & Victória da Silva Pacheco</h3>
+	<h4>Menu de Links</h4>
+	</div>
+	<body>			
 		<div class="menu">
-			<h2>Menu de Links</h2>
-			<div class="menu2">
-				<input id="botao-menu1" onclick="window.location.href = 'index.php'" style="text-align:center" class="botao4" name="botao" value="Cadastrar" />
-				<input id="botao-menu2" onclick="window.location.href = 'listabancoN2.php'" style="text-align:center" class="botao4" name="botao" value="Listar" />
-			</div>
+			<input id="botao-menu1" onclick="window.location.href = 'index.php'" style="text-align:center" name="botao" value="Cadastrar" />
+			<input id="botao-menu2" onclick="window.location.href = 'listabancoN2.php'" style="text-align:center"  name="botao" value="Listar" />
 		</div>
 	</body>
 </html>
